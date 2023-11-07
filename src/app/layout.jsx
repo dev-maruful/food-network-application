7ac3963b91833e82e981ecce2f2a91591e868e40
Tawsif Adnan <tawsif.adnan22@gmail.com>
2023-11-07 20:56:04 +0600

    json file added for fruits

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -13,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} max-w-7xl mx-auto`}>
+      <body className={`${inter.className}`}>
         <Header />
         <div className="flex">
           <Sidebar />
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,19 @@
+"use client";
+
 import {
   Cog6ToothIcon,
   ChevronUpIcon,
   ChevronDownIcon,
 } from "@heroicons/react/24/solid";
+import { useState } from "react";
 
 export default function Home() {
+  const [isSubtitleChecked, setIsSubtitleChecked] = useState(false);
+
+  const handleCheckboxChange = () => {
+    setIsSubtitleChecked(!isSubtitleChecked);
+  };
+
   return (
     <div className="flex gap-4">
       {/* Tabs section */}
@@ -22,7 +31,7 @@ export default function Home() {
             <div className="flex items-center gap-4">
               <Cog6ToothIcon className="h-5 w-5 color-primary" />
               <span className="color-primary text-sm font-semibold">
-                settings
+                Settings
               </span>
             </div>
             <ChevronUpIcon className="w-4 h-5 color-primary" />
@@ -31,23 +40,45 @@ export default function Home() {
           <div className="space-y-5">
             <div className="w-full space-y-2">
               <div className="flex items-center justify-between">
-                <p className="text-sm font-medium">Font size</p>
+                <p className="text-sm font-medium">Paragraph font size</p>
                 <p className="color-primary text-xs font-medium">19</p>
               </div>
               <input type="range" min={0} max="100" value="50" />
             </div>
             <div className="w-full">
-              <p className="text-sm font-medium mb-[10px]">Vitamin Type</p>
+              <p className="text-sm font-medium mb-[10px]">
+                Choose Vitamin Type
+              </p>
               <div className="bg-[#1010101A] p-2 rounded-md flex items-center justify-between">
                 <p>All Vitamin</p>
                 <ChevronDownIcon className="h-5 w-5" />
               </div>
             </div>
             <div className="w-full flex items-center justify-between">
-              <p className="text-sm font-medium">Subtitle</p>
-              <label className="switch">
-                <input type="checkbox" />
-                <span className="slider round"></span>
+              <p className="text-sm font-medium">Turn off Subtitle</p>
+              <label className="flex cursor-pointer select-none items-center">
+                <div
+                  className={`relative border rounded-[10px] ${
+                    isSubtitleChecked ? "border-none" : "border-gray-300"
+                  }`}
+                >
+                  <input
+                    type="checkbox"
+                    checked={isSubtitleChecked}
+                    onChange={handleCheckboxChange}
+                    className="sr-only"
+                  />
+                  <div
+                    className={`block h-[18px] w-[34px] rounded-full ${
+                      isSubtitleChecked ? "bg-color-primary" : "bg-transparent"
+                    }`}
+                  ></div>
+                  <div
+                    className={`absolute left-1 top-[2px] flex h-[14px] w-[14px] items-center justify-center rounded-full bg-white transition ${
+                      isSubtitleChecked ? "translate-x-full" : "bg-gray-400"
+                    }`}
+                  ></div>
+                </div>
               </label>
             </div>
           </div>
